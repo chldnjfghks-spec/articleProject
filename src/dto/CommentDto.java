ca9578c88187ccbc84c2631b73705095252288ba
chldnjfghks-spec <chldnjfghks@gmail.com>
2026-04-24 14:15:27 +0900

    feat : commentDto 구현 완료

--- src/dto/CommentDto.java
+++ src/dto/CommentDto.java
@@ -1,4 +1,72 @@
 package dto;
 
+
+import entity.Comment;
+
 public class CommentDto {
+    private Long commentId;
+    private Long articleId;
+    private String name;
+    private String content;
+
+    public CommentDto(Long commertId, Long articleid, String name, String content){
+        this.commentId = commertId;
+        this.articleId = articleid;
+        this.name = name;
+        this.content = content;
+    }
+
+    public Long getCommertId() {
+        return articleId;
+    }
+
+    public void setCommertId(Long commertId) {
+        this.commentId = commertId;
+    }
+
+    public Long getArticleid() {
+        return articleId;
+    }
+
+    public void setArticleid(Long articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public static CommentDto fromEntity(Comment comment){
+        return new CommentDto(
+                comment.getCommentId(),
+                comment.getArticleId(),
+                comment.getName(),
+                comment.getContent()
+        );
+    }
+
+    public static Comment fromDTO(CommentDto dto){
+        Comment comment = new Comment();
+        comment.setCommentId(dto.getCommertId());
+        comment.setArticleId(dto.getArticleid());
+        comment.setName(dto.getName());
+        comment.setContent(dto.getContent());
+        return comment;
+    }
+
+    @Override
+    public String toString() {
+        return "\t🏷️ " + commentId + "\t" + name + "\t" + content;
+    }
 }
